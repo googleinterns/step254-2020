4840d2d3c0fd98e171150bd4e79f2629de732015
ofarrero <ro.ofarrell1@gmail.com>
2020-08-06 13:21:23 +0000

    add set preference function

--- target/portfolio-1/script.js
+++ target/portfolio-1/script.js
@@ -19,9 +19,26 @@ function authenticate() {
   fetch(`/auth`).then(response => response.json()).then((authenticated) => {
     // Check if user has already been logged in.
     if (authenticated.email) {  
-      logInOut.innerHTML = `<a href="${authenticated.logoutUrl}">Logout</a>`;
+      window.location.href = "dashboard.html";
+      logInOut.innerHTML = `<a id= "login" href="${authenticated.logoutUrl}">Logout</a>`;    
     } else {
       logInOut.innerHTML = `<a href="${authenticated.loginUrl}">Login</a>`;
     }
   });
+}
+
+// Set user UI preferneces
+function setPrefernce(){
+  fetch('/authentication').then(response =>response.json()).then((authenticated) =>{
+    userFont = authenticated.font;
+    userFontSize = authenticated.font_size;
+    userFontColor = authenticated.text_color;
+    userBackgroundColor = authenticated.bg_color;
+
+    document.body.style.fontFamily = userFont;
+    document.body.style.fontSize = userFontSize + "px";
+    document.body.style.color = userFontColor;
+    document.body.style.backgroundColor = userBackgroundColor;
+  });
+
 }
