274dec7cb84a4cdc813fd5fe44d069f5f3123b87
Róisín O'Farrell <66687614+ofarrero@users.noreply.github.com>
2020-08-14 15:18:14 +0100

    Fix to conform with lint

--- src/main/webapp/test/preferenceFormDefault.test.js
+++ src/main/webapp/test/preferenceFormDefault.test.js
@@ -7,39 +7,40 @@ const exampleResponse = {
   text_color: 'black',
 };
 
-let setValueHtml =
+const setValueHtml =
     '<p id="font">Arial</p>' +
     '<p id="font_size">10</p>' +
     '<p id="text_color">Red</p>' +
-    '<p id="bg_color">Green</p>' ;
+    '<p id="bg_color">Green</p>';
 
-test ('check set value works with font', () => {
+test('check set value works with font', () => {
   document.body.innerHTML = setValueHtml;
   preferenceFormDefault.setValue('font', 'helvetica');
   expect(document.getElementById('font').value).toBe('helvetica');
 });
 
-test ('check set value works with font size', () => {
+test('check set value works with font size', () => {
   document.body.innerHTML = setValueHtml;
   preferenceFormDefault.setValue('font_size', '17');
   expect(document.getElementById('font_size').value).toBe('17');
 });
 
-test ('check set value works with text color', () => {
+test('check set value works with text color', () => {
   document.body.innerHTML = setValueHtml;
   preferenceFormDefault.setValue('text_color', 'black');
   expect(document.getElementById('text_color').value).toBe('black');
 });
 
-test ('check set value works with background color', () => {
+test('check set value works with background color', () => {
   document.body.innerHTML = setValueHtml;
   preferenceFormDefault.setValue('bg_color', 'white');
   expect(document.getElementById('bg_color').value).toBe('white');
 });
 
 test('check preference form servlet call works', async () => {
-    global.fetch = jest.fn().mockImplementation(() => Promise.resolve({json: () => exampleResponse}));
-    preferenceFormDefault.setPreferenceForm();
-    expect(global.fetch).toHaveBeenCalledWith('/auth');
-    expect((await global.fetch()).json()).toEqual(exampleResponse); 
-})
+  global.fetch = jest.fn().mockImplementation(() => 
+  Promise.resolve({json: () => exampleResponse}));
+  preferenceFormDefault.setPreferenceForm();
+  expect(global.fetch).toHaveBeenCalledWith('/auth');    
+  expect((await global.fetch()).json()).toEqual(exampleResponse);
+});
