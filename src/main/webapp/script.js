b2211553daa728cda05744f7d1a813bd12986d8b
AidanMolloy <aidan@molloy.ie>
2020-08-18 14:33:56 +0000

    Fixed eslint errors

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -28,7 +28,7 @@ async function authenticate() {
       userAuth = true;
       logInOut.innerHTML = `<a id= "login" href="${userDetails.logoutUrl}"
       >Logout</a>`;
-      setPreference(); 
+      setPreference();
     } else {
       userAuth = false;
       logInOut.innerHTML = `<a href="${userDetails.loginUrl}">Login</a>`;
@@ -65,7 +65,7 @@ async function setPreference() {
  * Check if user has access to page
  */
 function pageAccess() {
-    console.log(userName);
+  console.log(userName);
   if (userAuth === true) {
     const page = newUser(userName);
     console.log(page);
