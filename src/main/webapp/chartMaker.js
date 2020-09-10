7aecd7b3a7cb32f66ab1da919e6a3f3374fc6550
Róisín O'Farrell <66687614+ofarrero@users.noreply.github.com>
2020-09-10 11:45:19 +0100

    Update to comply with linter

--- src/main/webapp/chartMaker.js
+++ src/main/webapp/chartMaker.js
@@ -1,3 +1,17 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
 
 /** Import google charts */
 google.charts.load('current', {'packages': ['corechart']});
@@ -27,9 +41,10 @@ function drawPersonalResults() {
       });
 };
 
+/** Draw the student result as  col chart */
 function drawStudentResults() {
   // Gets exam data
-  fetch('/charts').then(response => response.json())
+   fetch('/charts').then((response) => response.json())
       .then((results) => {
         // Adds data from servlet to the chart
         const data = new google.visualization.DataTable();
@@ -37,41 +52,41 @@ function drawStudentResults() {
         data.addColumn('number', 'Frequency');
         data.addColumn({type: 'string', role: 'style'});
 
-        const style = 'stroke-color: #6A706E; stroke-width:' + 
+        const style = 'stroke-color: #6A706E; stroke-width:' +
               '4; fill-color: #82968C';
         Object.keys(results).forEach((finalMark) => {
           data.addRow([finalMark, results[finalMark], style]);
         });
 
-       // Adds basic styling to the chart
-       const options = {
-         title: 'Student Exam Results',
-         titleTextStyle: {
-           fontSize: 20,
-           bold: true,
-         },
-         width: 600,
-         height: 500,
-         hAxis: {
-           title: 'Marks recieved by Student',
-           titleTextStyle: {
-             fontSize: 17,
-             italic: true,
-           },
-         },
-         vAxis: {
-           title: 'No. of Students',
-           titleTextStyle: {
-             fontSize: 17,
-             italic: true,
-           },
-         },
-         legend: 'none',
-       };
+        // Adds basic styling to the chart
+        const options = {
+          title: 'Student Exam Results',
+          titleTextStyle: {
+            fontSize: 20,
+            bold: true,
+          },
+          width: 600,
+          height: 500,
+          hAxis: {
+            title: 'Marks recieved by Student',
+            titleTextStyle: {
+              fontSize: 17,
+              italic: true,
+            },
+          },
+          vAxis: {
+            title: 'No. of Students',
+            titleTextStyle: {
+              fontSize: 17,
+              italic: true,
+            },
+          },
+          legend: 'none',
+        };
 
-       // creates chart instance and populates it with data and adds the styling
-       const chart = new google.visualization.ColumnChart(
-         document.getElementById('chart-container'));
-         chart.draw(data, options);
-       });
+        // creates chart instance and populates it with data and adds the styling
+        const chart = new google.visualization.ColumnChart(
+          document.getElementById('chart-container'));
+          chart.draw(data, options);
+        });
 }
